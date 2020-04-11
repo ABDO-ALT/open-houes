2fc1d179e81ce431cd9f58da7848d98fad1792d4
Abdo Mohamad <abdo@AiralkhasbAbdo.home>
2020-04-11 18:17:32 +0200

    Create MainBar with style and add logo

---
+++ src/components/MainBar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "../App";
+import { Link } from "react-router-dom";
+
+function MainBar() {
+  return (
+    <div>
+      <header>
+        <div className="container">
+          <img src={"./logo.png"} alt="logo" className="logo" />
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default MainBar;
